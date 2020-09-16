18efc7ed86d62a75d0a44f13501077cafbe331a9
Ujjwal Goyal <importhuman@gmail.com>
2020-09-16 17:15:11 +0530

    Fixed edges and corners

--- grid.py
+++ grid.py
@@ -1,23 +1,54 @@
 import matplotlib.pyplot as plt 
 from matplotlib.animation import FuncAnimation
 import numpy as np 
-# import sys
+import sys
 
 
 #Force printing of entire array in terminal
-# np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
 
-grid = np.zeros((51,51), dtype=np.int16) #Create 100*100 array of zeroes
+grid = np.zeros((51,51), dtype=np.int16) #Create 51*51 array of zeroes (index 0 to 50)
+
 
 def neighbors(x,y):
 	#cell = grid[x,y]
-	n_cells = [grid[x-1, y+1], grid[x, y+1], grid[x+1, y+1], 
-				grid[x-1, y], grid[x+1, y], 
+	n_cells = []
+	#middle of the grid
+	if x in range(1,50) and y in range(1,50): 
+		n_cells = [grid[x-1, y+1], grid[x, y+1], grid[x+1, y+1], 
+					grid[x-1, y], grid[x+1, y], 
+					grid[x-1, y-1], grid[x, y-1], grid[x+1, y-1]]
+	#first column edge				
+	if x==0 and y in range(1,50):
+		n_cells = [grid[x, y+1], grid[x+1, y+1], 
+				grid[x+1, y], grid[x, y-1], grid[x+1, y-1]]
+	#last column edge
+	if x==50 and y in range(1,50):
+		n_cells = [grid[x-1, y+1], grid[x, y+1],
+				grid[x-1, y], grid[x-1, y-1], grid[x, y-1]]
+	#first row edge
+	if y==50 and x in range(1,50):
+		n_cells = [grid[x-1, y], grid[x+1, y], 
 				grid[x-1, y-1], grid[x, y-1], grid[x+1, y-1]]
+	#last row edge
+	if y==0 and x in range(1,50):
+		n_cells = [grid[x-1, y+1], grid[x, y+1], grid[x+1, y+1], 
+				grid[x-1, y], grid[x+1, y]]
+	#topleft corner
+	if x==0 and y==50:
+		n_cells = [grid[1,50], grid[0,49], grid[1,49]]
+	#topright corner
+	if x==50 and y==50:
+		n_cells = [grid[49,50], grid[49,49], grid[50,49]]
+	#bottomleft corner
+	if x==0 and y==0:
+		n_cells = [grid[0,1], grid[1,0], grid[1,1]]
+	#bottomright corner
+	if x==50 and y==0:
+		n_cells = [grid[50,1], grid[49,0], grid[49,1]]
 	return sum(n_cells)
 
-
-grid[0:40, 0:35]=1
+grid[0:50,0:50]=1
 
 
 #print(grid)
@@ -26,7 +57,7 @@ grid[0:40, 0:35]=1
 #Rules
 newgrid = grid.copy()
 def updatefig(i):
-	print(i)
+	# print(i) #Prints current iteration in terminal, for testing 
 	for x in range(50):
 		for y in range(50):
 			if grid[x,y]==0:
